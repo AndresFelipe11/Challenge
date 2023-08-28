dbe5cb44d505d4eb4d21dfc996d04fcf291e182f
AndresFelipe11m <andres.1701714898@gmail.com>
2023-08-28 12:30:24 -0500

    Feat: add package domain and email struct

---
+++ back/domain/email.go
@@ -0,0 +1,11 @@
+package domain
+
+type Email struct {
+	MessageID string `json:"message_id"`
+	Date      string `json:"date"`
+	From      string `json:"from"`
+	To        string `json:"to"`
+	Subject   string `json:"subject"`
+	Content   string `json:"content"`
+	Filepath  string `json:"filepath"`
+}
